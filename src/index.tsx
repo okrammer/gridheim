98396cbffe0e537b97c442bbd108700658f5aa85
okrammer <1805495+okrammer@users.noreply.github.com>
2019-11-07 09:36:02 +0100

    enabled flatmap shim

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import * as serviceWorker from "./serviceWorker";
 import "jquery";
 import "popper.js";
 import "bootstrap";
+import "array.prototype.flatmap";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
